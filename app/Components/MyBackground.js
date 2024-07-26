e632412ffa177878d6ea50819aadf5b5d9aee44a
Kuldeep Kushwaha <82083060+Kuldeep-kk@users.noreply.github.com>
2024-07-26 09:26:34 +0530

    Update MyBackground.js

--- app/Components/MyBackground.js
+++ app/Components/MyBackground.js
@@ -6,7 +6,7 @@ const MyBackground = ({ showSlider }) => {
     return (
         <motion.div
             className="absolute top-0 left-0 w-full h-full bg-cover bg-center -z-10"
-            style={{backgroundImage: "url('https://s3-alpha-sig.figma.com/img/e64b/5d51/202c7582fc54777bef345a3858aaf4d8?Expires=1717977600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=A4799xxUK5xfgnQ1qwvkKKSixSNiVgPwSWNLSpsEENBXNjBvpS0sHMyBSJ0iMGFu1TipzjbWA3F6BPWlMzEMKkXkiKzL0egNhybb~J9X883BNWZW3IDKIfrgZjbKj8R~~dd0-rOMNl17kH9D-T3OECbLZj2V0zTFsXaMapixcsUY2Bn19pGBBsNJGZxc1Vo4CUgKUPehkFPKCFapmvYtl1BCiEjJSies3PXjxZR5KE52UgFweyZ2qQ3bPkH5BBAwgkTrHI5nPCqpmV7q6pIAcMToR4EO9-NPDBbA3e-3YrYVt3tk6emKGZP~vNdGLFSyavzWDxvdZz6oRckQdRMw~Q__')"}}
+            style={{backgroundImage: "url('https://s3-alpha-sig.figma.com/img/e64b/5d51/202c7582fc54777bef345a3858aaf4d8?Expires=1722816000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nhPuEKLbNUYtDMkra3A-fOAp8VINi5dfmP7ekHLiRG4bd2MWOkdswkcU6bY3t69Clffd3~YhjjH~c0NjMic~iE-282NYIOvQ0EsmP7A7EUBnwzdFli~EjHTHEIx0UD2KQtCZHcYtbzUVkyeZbA8~q7cGYNaMGJXD8sug~AORVlIcY~mtSOjQ278prpYRIRv3X9p4AS6M5j72DnGx0YMz8mKdabCHfdNAzxX0-36L~kiaYsUwxgSXkFpd3Zy~illXLuWJIE~IIvz8vZRcyHQLNYhswUz3xPUo8oKldx8N69ZNVYiwrPKQLa~TOkEj9Wcj4xe4d~ZiBrvys9xsqK2iSg__')"}}
             animate={showSlider ? { scale:[1,1,1]} : { scale: [1, 1.12, 1] }}
             transition={showSlider ? {} : { duration: 10, ease: "easeInOut", repeat: Infinity }}
         >
